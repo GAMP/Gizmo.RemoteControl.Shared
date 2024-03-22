80ada78ee057b3dc917673a04df0a6cc9386cab5
Andrei Pestunov <masterlifting@gmail.com>
2024-03-22 07:44:13 +0100

    Add remote control session abstractions

---
+++ Interfaces/IRemoteControlSessionService.cs
@@ -0,0 +1,15 @@
+﻿using Gizmo.RemoteControl.Shared.Models;
+
+namespace Gizmo.RemoteControl.Shared.Interfaces
+{
+    public interface IRemoteControlSessionService
+    {
+        /// <summary>
+        /// Creates a remote control session.
+        /// <paramref name="hostId"/>Host id.</param>
+        /// <paramref name="cToken"/>Cancellation token.</param>
+        /// <returns>Remote control session.</returns>
+        /// </summary>
+        Task<RemoteControlSession> CreateSessionAsync(int hostId, CancellationToken cToken = default);
+    }
+}
---
+++ Models/RemoteControlSession.cs
@@ -0,0 +1,23 @@
+﻿namespace Gizmo.RemoteControl.Shared.Models
+{
+    /// <summary>
+    /// Represents a remote control viewer session.
+    /// </summary>
+    public sealed record RemoteControlSession
+    {
+        /// <summary>
+        /// Gets created remote session id.
+        /// </summary>
+        public Guid Id { get; init; }
+
+        /// <summary>
+        /// Gets create session id access key.
+        /// </summary>
+        public string AccessKey { get; init; } = null!;
+
+        /// <summary>
+        /// Gets the server url of the remote control host.
+        /// </summary>
+        public string Host { get; init; } = null!;
+    }
+}
